48001345f06f36b7ed82dac22dd0f0bb3a6ca7f9
nsieg <n.siegfried@gmx.de>
2022-02-15 23:07:23 +0100

    Changed to inline script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script type="text/javascript" src="/lib/matomo.js"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `var _paq=window._paq=window._paq||[];_paq.push(["trackPageView"]),_paq.push(["enableLinkTracking"]),function(){var e="//analytics.nilssiegfried.de/";_paq.push(["setTrackerUrl",e+"matomo.php"]),_paq.push(["setSiteId","1"]);var a=(t=document).createElement("script"),t=t.getElementsByTagName("script")[0];a.async=!0,a.src=e+"matomo.js",t.parentNode.insertBefore(a,t)}();`,
+            }}
+          />
         </Head>
         <body>
           <Main />
